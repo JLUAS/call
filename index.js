4bb622590de72571373eb8a7496207e41c86a2a2
JLUAS <jlurquieta1@gmail.com>
2024-12-06 11:39:23 -0600

    prueb

--- index.js
+++ index.js
@@ -300,11 +300,10 @@ fastify.register(async (fastify) => {
     });
 });
 
-const port = process.env.PORT || 3000;
-fastify.listen(port, '0.0.0.0', (err, address) => {
+fastify.listen({ port: PORT }, (err) => {
     if (err) {
         console.error(err);
         process.exit(1);
     }
-    console.log(`Server listening at ${address}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
