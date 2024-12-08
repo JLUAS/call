1d77c6bcd7a1e4be3c678e71a83d1296b9ddffc5
JLUAS <jlurquieta1@gmail.com>
2024-12-07 18:49:41 -0600

    cambio en call

--- index.js
+++ index.js
@@ -150,7 +150,7 @@ app.post("/voice", async (req, res) => {
 app.post('/make-call', (req, res) => {
   io.emit("make-call", +528662367673)
   console.log("hola?")
-  res.status(200).send({ message: 'Llamada realizada con éxito', callSid: call.sid });
+  res.status(200).send({ message: 'Llamada realizada con éxito' });
 })
 
 // Iniciar servidor
